60e9ea0074e8f9b3eb63af92aec2689f20564ecf
kevinhes <kevinhes1115@gmail.com>
2025-01-03 11:46:32 +0800

    初始化 Vue 專案，新增基本檔案與配置

---
+++ src/App.vue
@@ -0,0 +1,204 @@
+<script setup>
+</script>
+
+<template>
+  <section class="resume-body">
+    <header class="mb-3">
+      <h1 class="mb-2">姜宥先</h1>
+      <h4 class="mb-2">目標職位：前端工程師</h4>
+      <p class="mb-2">兩年網站開發經驗，完成約 30 個專案，專注於 WordPress 客製化主題設計，並在部分專案中應用 React 技術。期間自學並掌握 Vue 和 Nuxt，具備基礎實踐經驗，同時熟悉 Webpack 和 Vite 等現代前端工具。希望加入以 Vue 和 Nuxt 為核心技術的團隊，發揮專長並持續成長。</p>
+      <ul class="list-unstyled contact-list">
+        <li>
+          <a href="https://github.com/kevinhes">
+            <i class="bi bi-github"></i>
+            <span>
+              github.com/kevinhes
+            </span>
+          </a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/kevinhes">
+            <i class="bi bi-linkedin"></i>
+            <span>
+              www.linkedin.com/in/kevinhes
+            </span>
+          </a>
+        </li>
+        <li>
+          <a href="mailto:kevinhes1115@gmail.com">
+            <i class="bi bi-envelope-fill"></i>
+            <span>
+              kevinhes1115@gmail.com
+            </span>
+          </a>
+        </li>
+        <li>
+          <a href="tel:+0939374673">
+            <i class="bi bi-telephone-fill"></i>
+            <span>
+              0939374673
+            </span>
+          </a>
+        </li>
+      </ul>
+    </header>
+    <main>
+      <!-- skill -->
+      <h2 class="mb-3">技能</h2>
+      <section class="skill-list">
+        <div class="">
+          <h4 class="text-dark mb-2 fw-bold">核心前端技能</h4>
+          <ul class="list-unstyled">
+            <li>熟悉 HTML、CSS，運用 SCSS 提升維護性。</li>
+            <li>規劃 RWD 網站，熟練使用 Bootstrap 快速開發。</li>
+            <li>使用 JavaScript (ES6) 開發互動效果，具備 API 串接經驗。</li>
+          </ul>
+        </div>
+        <div class="">
+          <h4 class="text-dark mb-2 fw-bold">框架與工具</h4>
+          <ul class="list-unstyled">
+            <li>使用 Vue 3 開發組件，熟悉 Composition API。</li>
+            <li>在 Nuxt 3 中構建 SSR 網站，增強 SEO 效能。</li>
+            <li>使用 Pinia 管理狀態。</li>
+            <li>曾用 React 開發動態表單與多步驟功能。</li>
+          </ul>
+        </div>
+        <div class="">
+          <h4 class="text-dark mb-2 fw-bold">開發工具</h4>
+          <ul class="list-unstyled">
+            <li>基本掌握 Git，用於版本控制。</li>
+            <li>使用 Webpack 和 Vite 進行資源打包。</li>
+          </ul>
+        </div>
+      </section>
+      <!-- portfolio -->
+      <h2 class="mb-3">作品集</h2>
+      <section class="mb-2">
+        <ul class="portfolio-list list-unstyled">
+          <!-- <li class="list-item mb-3">
+            <div class="item-header">
+              <h4 class="text-dark fw-bold mb-2">DCDC 生髮診所</h4>
+              <p>PHP、React、SCSS</p>
+            </div>
+            <ul class="list-unstyled item-content">
+              
+              <li>運用 React 開發多選項聯動選單，相較傳統方法縮短約 30% 開發時間</li>
+              <li>網站連結：<a class="d-inline" href="https://dcdchair.com.tw/">https://dcdchair.com.tw/</a></li>
+            </ul>
+          </li> -->
+          <li class="list-item mb-3">
+            <div class="item-header">
+              <h4 class="text-dark fw-bold mb-2">台北靈糧堂</h4>
+              <p>PHP、cURL、SCSS、WordPress</p>
+            </div>
+            <ul class="list-unstyled item-content">
+              <li>使用 SCSS 客製化樣式與 Bootstrap，並利用 PHP 模板重用區塊，提升設計一致性與開發效率。</li>
+              <li>開發 WordPress 外掛，讀取 CSV 檔案自動匯入文章，減少約 70% 的手動操作時間。</li>
+              <li>客製化 Podcast 播放器，整合 RSS 數據，優化用戶收聽體驗。</li>
+              <li class="mb-2">使用 PHP 的 cURL 功能，串接並抓取課程資料。</li>
+              <li>網站連結：<a class="d-inline" href="https://www.breadoflife.taipei/">https://www.breadoflife.taipei/</a></li>
+            </ul>
+          </li>
+          <li class="list-item">
+            <div class="item-header">
+              <h4 class="text-dark fw-bold mb-2">訂房網站 - Side Project</h4>
+              <p>Nuxt 3、Pinia、SSR、RESTful API</p>
+            </div>
+            <ul class="list-unstyled item-content">
+              <li>使用 Nuxt 3 與 SSR 模式開發，提升 SEO 與效能。</li>
+              <li>利用 Pinia 管理狀態，實現 API 資料處理與同步。</li>
+              <li class="mb-2">開發會員系統與線上訂房功能，支援多種資料操作（GET、POST、PUT、DELETE）。</li>
+              <li>網站連結：<a class="d-inline" href="https://hotel-officail.onrender.com">https://hotel-officail.onrender.com</a></li>
+              <li>Github 連結：<a class="d-inline" href="https://github.com/kevinhes/hotel-nuxt">https://github.com/kevinhes/hotel-nuxt</a></li>
+            </ul>
+          </li>
+        </ul>
+      </section>
+      <!-- 經歷 -->
+      <h2 class="mb-3">經歷</h2>
+      <section class="mb-3">
+        <ul class="portfolio-list list-unstyled">
+          <li class="list-item mb-3">
+            <div class="item-header">
+              <a href="www.design-hu.com">
+                <h4 class="text-dark fw-bold mb-2">鵠崙設計有限公司</h4>
+              </a>
+              <p class="mb-2">2022 - 2024</p>
+              <p class="mb-2">前端工程師</p>
+            </div>
+            <ul class="list-unstyled item-content">
+              <li>負責 WordPress 客製化主題的開發與專案交付，完成超過 30 個企業形象網站與電商網站。</li>
+              <li>使用 SCSS 優化樣式管理，提升設計一致性並減少後續維護成本。</li>
+              <li>部分專案中整合 React 技術，開發動態互動表單與模組化元件。</li>
+              <li>開發自動化工具（如 WordPress 外掛），縮短資料輸入與處理時間，提升團隊效率約 70%。</li>
+            </ul>
+          </li>
+        </ul>
+      </section>
+    </main>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+  .skill-list li {
+    display: flex;
+    align-items: center;
+  }
+  .portfolio-list {
+    .list-item {
+      display: flex;
+      justify-content: space-between;
+      .item-header {
+        width: 30%;
+      }
+      .item-content {
+        width: 66%;
+      }
+    }
+  }
+  // .portfolio-list li {
+  //   display: flex;
+  //   justify-content: space-between;
+  //   :first-child {
+  //     width: 30%;
+  //   }
+  //   :last-child {
+  //     width: 68%;
+  //     li {
+  //       width: 100%;
+  //     }
+  //   }
+  // }
+
+  .bi {
+    color: #6b8e23;
+    margin-right: 8px;
+    font-size: 20px;
+  }
+
+  a {
+    display: flex;
+    align-items: center;
+  }
+
+  .contact-list {
+    display: grid;
+    gap: 4px;
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  // .w-31 {
+  //   width: 31%;
+  // }
+
+  .skill-list {
+    display: grid;
+    gap: 16px;
+    grid-template-columns: repeat(3, 1fr);
+    // grid-template-rows: repeat(3, 1fr);
+  }
+
+  .bg-c-primary {
+    background-color: #6b8e23;
+  }
+</style>
